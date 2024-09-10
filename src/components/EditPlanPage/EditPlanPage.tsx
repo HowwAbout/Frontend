6e99db7f16d2bd5f7a0aac17f818e6c830df5f27
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-09-10 17:09:42 +0900

    [bug] 컴마 수정

--- src/components/EditPlanPage/EditPlanPage.tsx
+++ src/components/EditPlanPage/EditPlanPage.tsx
@@ -41,7 +41,7 @@ const EditPlanPage: React.FC = () => {
           "엔티크 와인 바의 야외 루프탑에서 “옥상 수비드 부채살 스테이크”와 레드와인을 곁들여 즐기기",
         dateTime: "1시간 30분",
         activityDescription:
-          "분위기 좋은 성북구에 위치한 엔티크 와인 바의 야외 루프탑에서 “옥상 수비드 부채살 스테이크”와 레드와인을 곁들여 즐기기",
+          "분위기 좋은 성북구에 위치한 엔티크 와인 바의 야외 루프탑에서 “옥상 수비드 부채살 스테이크”와 레드와인을 곁들여 즐기기"
       };
 
       const response = await axios.post(url, data);
